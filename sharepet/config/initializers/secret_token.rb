c5a44d93b47eb66f2d8edc6a49dcebb1a22a13cc
Luiz Oliveira <ziuloliveira@gmail.com>
2015-11-07 14:51:41 -0200

    First step
    Signed-off-by: Luiz Oliveira <ziuloliveira@gmail.com>

---
+++ sharepet/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sharepet::Application.config.secret_token = '7af89240e64e0e32f3cb9df06413185c3bbec4fab9b37dc6c617fcce36a129e63ee2d2ad4aca5ca074e9d8d3e2b42193ed47705b4c5728876fbc54f080b7ded7'
