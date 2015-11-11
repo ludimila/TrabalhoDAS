0c152a1779b7076ea2a2bb46c3f4c7209a9f89ba
Luiz Oliveira <ziuloliveira@gmail.com>
2015-11-11 14:04:58 -0200

    Filtering by breed
    Closes #12 with it
    
    Signed-off-by: Luiz Oliveira <ziuloliveira@gmail.com>

--- sharepet/app/models/animal.rb
+++ sharepet/app/models/animal.rb
@@ -1,4 +1,5 @@
 class Animal < ActiveRecord::Base
   attr_accessible :age, :animalType, :breed, :name
-  scope :by_breed, -> breed { where(:breed => breed) }
+  # scope :by_breed, -> breed { where( :breed => breed) }
+  scope :by_breed, -> breed { where( "lower(animals.breed) like lower(?)", "%#{breed}%") }
 end
