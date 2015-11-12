02bfd00500796ce5445aebb93f2921cb87ac12c8
Macário Soares <macario.junior@gmail.com>
2015-11-12 01:24:42 -0200

    Creating mailer to system mail reports

--- sharepet/app/mailers/report_mailer.rb
+++ sharepet/app/mailers/report_mailer.rb
@@ -1,9 +1,9 @@
 class ReportMailer < ActionMailer::Base
   default from: "sharepet3@gmail.com"
 
-  def wellcomeMail(user)
-  	@user = user
-  	mail(:to=> user.mail, :subject=>"Wellcome to SharePet!")
+  def sharingPetMail(animal)
+  	@animal = animal
+  	mail(:to=> "user_email@example.com", :subject=>"Thanks for sharing a pet!")
   end
-  
+
 end
