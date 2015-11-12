d73fd02f4ea6a0cef953fcc0a74f7874e96401fd
tiagorc <tiagobrasil2005@yahoo.com.br>
2015-11-12 06:44:13 -0400

    scaffolding user

--- sharepet/test/unit/user_test.rb
+++ sharepet/test/unit/user_test.rb
@@ -4,4 +4,17 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+	def setup
+		@user = User.new(name: "UserTest", email: "user@email.com", username: "user", password: "password")
+	end
+
+	test "should be valid" do
+		assert @user.valid?
+	end
+
+	test "name should be present" do
+		@user.name =  "    "
+		assert_not @user.valid?
+	end
 end
