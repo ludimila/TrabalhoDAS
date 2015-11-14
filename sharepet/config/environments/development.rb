2f04103ea7f984c273ceae05a7f740d244255a50
saasbook <saasbook@saasbook.(none)>
2015-11-14 13:16:39 -0400

    Image upload almost finished

--- sharepet/config/environments/development.rb
+++ sharepet/config/environments/development.rb
@@ -6,6 +6,9 @@ Sharepet::Application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
+  # PaperClip Integration
+  Paperclip.options[:comand_path] = "/usr/local/bin/"
+
   # Log error messages when you accidentally call methods on nil.
   config.whiny_nils = true
 
