bafa2d4d12e917c69272f667fe31defb74a1eeba
saasbook <saasbook@saasbook.(none)>
2015-11-14 13:16:39 -0400

    Image upload almost finished

--- sharepet/app/models/animal.rb
+++ sharepet/app/models/animal.rb
@@ -3,4 +3,7 @@ class Animal < ActiveRecord::Base
   # scope :by_breed, -> breed { where( :breed => breed) }
   scope :by_breed, -> breed { where( "lower(animals.breed) like lower(?)", "%#{breed}%") }
   scope :only_available, -> { where(adopted: false) }
+  attr_accessor :picture_file_name
+  attr_accessor :picture_content_type
+  attr_accessor :picture_file_size
 end
