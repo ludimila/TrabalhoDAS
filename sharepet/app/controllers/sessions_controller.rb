673e898e2e7c7f0214ca2d00e046b5269e8c7f0a
tiagorc <tiagobrasil2005@yahoo.com.br>
2015-11-21 14:43:26 -0400

    user logged in and logged out, fixed view user

--- sharepet/app/controllers/sessions_controller.rb
+++ sharepet/app/controllers/sessions_controller.rb
@@ -23,7 +23,11 @@ class SessionsController < ApplicationController
 
 
   def current_user
-  	@current_user ||= User.find_by(id: session[:user_id])
+  	@current_user ||= User.where(id: session[:user_id]).first
+  end
+
+  def logged_in?
+    !current_user.nil?
   end
 
 end
