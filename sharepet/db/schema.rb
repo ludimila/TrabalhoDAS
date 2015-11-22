422dc8912407d5e02d09f697b1fce6c71eee0a90
Ruyther Costa <ruy_fusion@hotmail.com>
2015-11-22 09:22:34 -0400

    Adding migration for animals

--- sharepet/db/schema.rb
+++ sharepet/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20151116040829) do
+ActiveRecord::Schema.define(:version => 20151122125044) do
 
   create_table "animals", :force => true do |t|
     t.string   "name"
@@ -20,16 +20,26 @@ ActiveRecord::Schema.define(:version => 20151116040829) do
     t.integer  "age"
     t.datetime "created_at",                              :null => false
     t.datetime "updated_at",                              :null => false
-    t.string   "address"
-    t.float    "latitude"
-    t.float    "longitude"
-    t.boolean  "adopted",              :default => false
+    t.string   "attach_file_name"
+    t.string   "attach_content_type"
+    t.integer  "attach_file_size"
+    t.datetime "attach_updated_at"
+    t.string   "pic_file_name"
+    t.string   "pic_content_type"
+    t.integer  "pic_file_size"
+    t.datetime "pic_updated_at"
     t.string   "picture"
     t.string   "picture_file_name"
     t.string   "picture_content_type"
     t.integer  "picture_file_size"
     t.datetime "picture_updated_at"
+    t.string   "address"
+    t.float    "latitude"
+    t.float    "longitude"
+    t.boolean  "adopted",              :default => false
     t.string   "attachment"
+    t.string   "donor"
+    t.string   "interested"
   end
 
   create_table "users", :force => true do |t|
@@ -43,7 +53,7 @@ ActiveRecord::Schema.define(:version => 20151116040829) do
     t.datetime "created_at",      :null => false
     t.datetime "updated_at",      :null => false
     t.string   "password_digest"
-    t.string   "attachment"
+    t.string   "attachement"
   end
 
   add_index "users", ["username"], :name => "index_users_on_username", :unique => true
