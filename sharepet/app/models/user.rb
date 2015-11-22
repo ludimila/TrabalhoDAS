102d713f9665d1d7f3c72b59c297bbb866bf40ce
Caique Pereira <caiquepereira@gmail.com>
2015-11-22 08:32:33 -0400

    Added find or create auth hash to user model

--- sharepet/app/models/user.rb
+++ sharepet/app/models/user.rb
@@ -5,4 +5,18 @@ class User < ActiveRecord::Base
   validates :email, presence: true
   validates :password, presence: true
   has_secure_password
+
+def self.find_or_create_from_auth_hash(auth_hash)
+  	user = where(provider: auth_hash.provider, uid: auth_hash.uid).first_or_create
+  	user.update_attributes(
+  		name: auth_hash.info.name,
+  		profile_image: auth_hash.info.image,
+  		token: auth_hash.credentials.token,
+  		secret: auth_hash.credentials.secret,
+     	email: "email twitter",
+     	password: "senha twitter"
+  		)
+  	user
+  end
+
 end
