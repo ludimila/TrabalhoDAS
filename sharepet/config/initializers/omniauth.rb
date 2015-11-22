ee4178dcc2c2d5b7e8798e513c4400c63b8eddfb
Caique Pereira <caiquepereira@gmail.com>
2015-11-22 08:02:24 -0400

    Added secret and twitter key

--- sharepet/config/initializers/omniauth.rb
+++ sharepet/config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-	provider :twitter
+	provider :twitter, 'CjnwzLIJ9M8fuag7rjtghPuvM', 'qj4X7A373kguT6gJtoi2kIqBtZ0xDWS6WPxvEPQUAvkjGT08nZ'
 end
