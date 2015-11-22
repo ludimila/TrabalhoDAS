2d38d7e47fd895b3e3508af9bf579e285388ad20
Caique Pereira <caiquepereira@gmail.com>
2015-11-22 15:09:15 -0400

    Added username on update method when login with twitter

--- sharepet/app/models/user.rb
+++ sharepet/app/models/user.rb
@@ -10,6 +10,7 @@ def self.find_or_create_from_auth_hash(auth_hash)
   	user = where(provider: auth_hash.provider, uid: auth_hash.uid).first_or_create
   	user.update_attributes(
   		name: auth_hash.info.name,
+      username: auth_hash.info.nickname,
   		profile_image: auth_hash.info.image,
   		token: auth_hash.credentials.token,
   		secret: auth_hash.credentials.secret,
