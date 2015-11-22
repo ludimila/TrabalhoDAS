d59b1c3e171032175732dacfaf23384b8ff78fc5
Caique Pereira <caiquepereira@gmail.com>
2015-11-22 08:19:24 -0400

    Added attributes from twitter to user model

--- sharepet/app/models/user.rb
+++ sharepet/app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  attr_accessible :address, :email, :name, :password, :phone_number, :username, :zip_code, :attachment
+  attr_accessible :address, :email, :name, :password, :phone_number, :username, :zip_code, :attachment, :profile_image, :token, :secret, :provider, :uid
   mount_uploader :attachment, AttachmentUploader
   validates :name, presence: true
   validates :email, presence: true
