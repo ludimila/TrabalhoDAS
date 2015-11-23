aba7a7834c840646683dabf0eae495bff602b9d6
Caique Pereira <caiquepereira@gmail.com>
2015-11-23 15:28:42 -0400

    Updated tweet method

--- sharepet/app/models/user.rb
+++ sharepet/app/models/user.rb
@@ -2,7 +2,6 @@ class User < ActiveRecord::Base
   attr_accessible :address, :email, :name, :password, :phone_number, :username, :zip_code, :attachment, :profile_image, :token, :secret, :provider, :uid
   mount_uploader :attachment, AttachmentUploader
   validates :name, presence: true
-  validates :email, presence: true
   validates :password, presence: true
   has_secure_password
 
@@ -14,20 +13,22 @@ def self.find_or_create_from_auth_hash(auth_hash)
   		profile_image: auth_hash.info.image,
   		token: auth_hash.credentials.token,
   		secret: auth_hash.credentials.secret,
-     	email: "email twitter",
+     	email: nil,
      	password: "senha twitter"
   		)
   	user
   end
 
-def twitter
+def tweet(tweet)
   @client ||= Twitter::REST::Client.new do |config|
    config.consumer_key        = "CjnwzLIJ9M8fuag7rjtghPuvM"
    config.consumer_secret     = "qj4X7A373kguT6gJtoi2kIqBtZ0xDWS6WPxvEPQUAvkjGT08nZ"
    config.access_token        = token
    config.access_token_secret = secret
   end
+  @client.update(tweet)
 end
+
 
 
 end
