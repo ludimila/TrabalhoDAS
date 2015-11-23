bd22857573473efec8d75e912d23e711e34a4855
Caique Pereira <caiquepereira@gmail.com>
2015-11-23 15:25:51 -0400

    Created method that tweets the animal

--- sharepet/app/controllers/animals_controller.rb
+++ sharepet/app/controllers/animals_controller.rb
@@ -18,11 +18,18 @@ class AnimalsController < ApplicationController
     end
   end
 
+    def tweet_animal
+    # @animal = Animal.find(params[:id])
+    string = "Adote esse animal domestico: " + "http://0.0.0.0:3000/animals/" + "id"   #@animal.id.to_s
+    tweet = current_user.tweet(string)
+    redirect_to "/animals"
+  end
+
+
   # GET /animals/1
   # GET /animals/1.json
   def show
     @animal = Animal.find(params[:id])
-
     respond_to do |format|
       format.html # show.html.erb
       format.json { render json: @animal }
