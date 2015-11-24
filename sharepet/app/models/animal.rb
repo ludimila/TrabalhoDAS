c2285f3c8fd200aaa8e0cf51f58d43c546a1ed17
ludimila <lbelacruz@gmail.com>
2015-11-23 20:36:07 -0400

    adicionanfo filtro por endereco

--- sharepet/app/models/animal.rb
+++ sharepet/app/models/animal.rb
@@ -6,6 +6,7 @@ class Animal < ActiveRecord::Base
   # scope :by_breed, -> breed { where( :breed => breed) }
   mount_uploader :attachment, AttachmentUploader # Tells rails to use this uploader
   scope :by_breed, -> breed { where( "lower(animals.breed) like lower(?)", "%#{breed}%") }
+  scope :by_address, -> address { where( "lower(animals.address) like lower(?)", "%#{address}%") }
   scope :only_available, -> { where(adopted: false) }
   geocoded_by :address
   after_validation :geocode, :if => :address_changed?
